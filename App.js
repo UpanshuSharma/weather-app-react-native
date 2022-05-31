98cf380d630a4f746da917e0fea3f2c48392a7d3
Upanshu Sharma <upanshupathak@gmail.com>
2022-05-31 15:47:48 +0530

    weather forecast app using react native

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import {SafeAreaView} from 'react-native';
+
+import NavigationComponent from './lib/component/Navigation';
+const App =()=>{
+  return(
+          <NavigationComponent/>
+  )
+}
+
+export default App;
